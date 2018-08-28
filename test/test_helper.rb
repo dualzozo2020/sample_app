5c0faa383a497c162267a3049ad31810f8a40c5a
zozo2020 <dualzozo2020@gmail.com>
2018-08-28 21:29:33 +0000

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,15 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
+ENV['RAILS_ENV'] ||= 'test'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
   # Add more helper methods to be used by all tests here...
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
